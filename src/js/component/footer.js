152270f5cb2f6449b7bbe57ea6240b13504e7f04
Agustín Ruiz <143494692+GoldenDrk@users.noreply.github.com>
2024-01-31 18:49:27 +0000

    More color change

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center fixed-bottom">
-		<p>
+		<p className="text-light">
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
 			<a href="http://www.4geeksacademy.com">Agustín Ruiz</a>
 		</p>
